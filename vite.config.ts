54cb6ed0fed3dff3ed43e53cfea2a7828c94381e
vyaslav <vyaslav@gmail.com>
2025-07-20 13:25:33 +0200

    fix root path

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dual-n-back.github.io/',
+  base: '/',
   build: {
     outDir: 'dist',
     sourcemap: true,
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          mui: ['@mui/material', '@mui/icons-material'],
+        },
+      },
+    },
   },
   server: {
     port: 3000,
